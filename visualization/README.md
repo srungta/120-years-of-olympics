229a49cdccf60079e11563347d065bdbccee35aa
Shaswat Rungta <shaswatrungta@hotmail.com>
2018-08-26 15:58:59 +0530

    - Adding uniform logger setup method - Added script usage in Readme.md - Ignoring .log files in the repo. If nay log file needs to be commited, use -f flag in commit

---
+++ visualization/README.md
@@ -0,0 +1,5 @@
+## How to use this script.
+- Run the Elasticsearch and Kibana instance as mentioned in the root Readme file.
+- Run `python setup.py -h` to see how to use the script.
+- If you are following the path structure of this repository, you can use
+> python setup.py ..\data\athlete_events.csv ..\data\noc_regions.csv olympics -c 1000 -v -l indexinglogfile.log -d av-lp
--- visualization/setup.py
+++ visualization/setup.py
@@ -2,24 +2,61 @@ import argparse
 import pandas as pd
 from elasticsearch import Elasticsearch
 import logging
+import sys
 logging.basicConfig(level=logging.DEBUG)
 
+def setup_custom_logger(name, logfilename):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.FileHandler(logfilename, mode='w')
+    handler.setFormatter(formatter)
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+    return logger
 
 def main(athletefile, nocfile,
-         indexname, chunksize, doctype, deleteexisting, verbose):
-    logger = logging.getLogger(name=__name__)
+         indexname, chunksize, doctype, deleteexisting, verbose, logfilename):
+    logger = setup_custom_logger(name=__name__, logfilename=logfilename)
     if verbose == False:
         logger.disabled = True
     logger.info("All arguments received. Starting to index.")
-    logger.debug("Reading athlete file.")
-    athlete_events = pd.read_csv(athletefile, chunksize=chunksize)
-    logger.debug("Athlete file read in a dataframe.")
-    logger.debug("Reading noc file.")
-    noc_file = pd.read_csv(nocfile, chunksize=chunksize)
-    logger.debug("Noc file read in a dataframe.")
-    logger.debug("Initializing elastic search.")
-    logger.disabled = False
 
+    logger.debug("Reading athlete file.")
+    athlete_events = pd.read_csv(athletefile, iterator=True,chunksize=chunksize)
+    logger.debug("Athlete file read in a dataframe.")
+
+    logger.debug("Reading noc file.")
+    noc_file = pd.read_csv(nocfile,iterator=True, chunksize=chunksize)
+    logger.debug("Noc file read in a dataframe.")
+
+    logger.info("Initializing elastic search.")
+    es = Elasticsearch(timeout=30)
+    if deleteexisting:
+        try:
+            logger.debug("Trying to delete the index.")
+            es.indices.delete(index=indexname)
+            logger.debug("Deleted the index.")
+        except Exception as e:
+            logger.error('Could not delete index.',e)
+    logger.debug("Trying to create the index.")
+    es.indices.create(index=indexname)
+    logger.debug("Index created successfully.")
+    for i, df in enumerate(athlete_events):
+        logger.debug("Iteration" + str(i) + " : dataframe size "+ str(len(df.index)))
+        records = df.where(pd.notnull(df),None).T.to_dict()
+        list_records = [records[value] for value in records]
+        for record in list_records:
+            try:
+                es.index(indexname, doctype, body=record)
+            except:
+                logger.error(sys.exc_info()[0])
+                pass
+    logger.debug("Indexing complete.")
+    logger.disabled = False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -39,7 +76,8 @@ if __name__ == '__main__':
                         help='Doc type for the search index.')
     parser.add_argument('--deleteexisting', '-de', action='store_false',
                         help='If set to True, existing indices will be deleted.')
-
+    parser.add_argument('--logfilename', '-l', default='log.log',
+                        help='Path to the log file.')
     args = parser.parse_args()
     main(args.athletefile, args.nocfile,
-         args.indexname, args.chunksize, args.doctype, args.deleteexisting, args.verbose)
+         args.indexname, args.chunksize, args.doctype, args.deleteexisting, args.verbose, args.logfilename)
